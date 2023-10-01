0aa3d7ee7e1086f4abdda2dbb9c365915a93d356
Ryan Anderson <anderson.ryan.w@live.com>
2023-10-01 13:26:15 -0500

    Convert to system

--- src/sheets/actor/ActorSheetTemplate.svelte
+++ src/sheets/actor/ActorSheetTemplate.svelte
@@ -1,9 +1,19 @@
 <script>
+   import { ApplicationShell } from '#runtime/svelte/component/core';
+
+   export let elementRoot;
 </script>
 
-<main class="d4eredoSheet">
-   <h1>Test!</h1>
-</main>
+<!-- This is necessary for Svelte to generate accessors TRL can access for `elementRoot` -->
+<svelte:options accessors={true}/>
+
+<!-- ApplicationShell provides the popOut / application shell frame, header bar, content areas -->
+<!-- ApplicationShell exports `elementRoot` which is the outer application shell element -->
+<ApplicationShell bind:elementRoot>
+   <main>
+      <h1>Basic Character Sheet</h1>
+   </main>
+</ApplicationShell>
 
 <style lang="scss">
    h1 {
--- src/sheets/actor/d4eredoActorSheet.js
+++ src/sheets/actor/d4eredoActorSheet.js
@@ -1,64 +1,25 @@
-import { SvelteApplication } from '@typhonjs-fvtt/runtime/svelte/application';
+import { SvelteApplication } from '#runtime/svelte/application';
 // import { TJSDocument } from '@typhonjs-fvtt/runtime/svelte/store/fvtt';
 import ActorSheetTemplate from './ActorSheetTemplate.svelte';
 
 export default class d4eredoActorSheet extends SvelteApplication {
-   #component; // Reference to the inner Svelte sheet template
-   object; // Reference to the document object (Actor, Item, etc..)
-   #document; // Reference to the TJSDocument object that contains object.
+   /**
+    * Default Application options
+    *
+    * @returns {object} options - Application options.
+    * @see https://foundryvtt.com/api/Application.html#options
+    */
+   static get defaultOptions()
+   {
+      return foundry.utils.mergeObject(super.defaultOptions, {
+         title: '4eRedo.actor.title',  // Automatically localized from `lang/en.json`.
+         width: 300,
+         height: 600,
 
-   // Svelte template
-   get template() {
-      return this.svelte.applicationShell;
+         svelte: {
+            class: ActorSheetTemplate,
+            target: document.body
+         }
+      });
    }
-
-   constructor(
-      object = {},      // This is typically passed in by Foundry
-      options = {},     // This is typically passed in by Foundry
-      component = false // Sheets derived from d4eredoBaseSheet need to pass
-   ) {
-      super(options);   // SvelteApplication doesn't need object.
-
-      // Save our document object for later use.
-      this.object = object;
-
-      // Create a new TJSDocument using the document object pass in from Founry.
-      // This is so that the Svelte component updates reactively.
-      // this.#document = new TJSDocument(object, {
-      //    delete: this.close.bind(this),
-      // });
-
-      // Store the inner sheet template component.
-      this.#component = component;
-   }
-
-    /**
-     * Default Application options
-     *
-     * @returns {object} options - Application options.
-     * @see https://foundryvtt.com/api/Application.html#options
-     */
-    static get defaultOptions() {
-        return foundry.utils.mergeObject(super.defaultOptions, {
-            title: "4eRedo.title", // Automatically localized from `lang/en.json`.
-            width: 600,
-            height: 400,
-
-            svelte: {
-                class: ActorSheetTemplate, // Svelte base template
-                target: document.body, // Where we are rendering the sheet
-                props: function() {
-                    // This allows us to load in default
-                    // properties to the sheet.
-                    return {
-                        // We'll pass in the sheet and the document object
-                        sheet: this,
-                        doc: this.#document,
-                        // The inner sheet template to render
-                        component: this.#component,
-                    };
-                },
-            },
-        });
-    }
 }
